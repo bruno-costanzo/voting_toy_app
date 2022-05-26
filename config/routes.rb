22b6eac26ea454429199387a0e74d018327083ce
bruno-costanzo <dev.bcostanzo@gmail.com>
2022-05-26 12:55:19 -0300

    root added

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root "referendas#index"
+
   post 'votes/create', to: 'votes#create'
   resources :referendas
   resources :people
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
